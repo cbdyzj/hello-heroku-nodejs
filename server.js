d4f7711cd196b540bbb4126e1b07f8914151bdd4
Cbdy <cbdyzj@gmail.com>
2017-08-05 14:00:11 +0800

    add port setting

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const path = require('path')
 const http = require('http')
 
+const port = process.env.PORT || 8080
+
 const app = express()
 app.use(express.static(path.join(__dirname, 'public')))
-http.createServer(app).listen(80, () => console.info('server listening'))
+http.createServer(app).listen(port, () => console.info(`server listening ${port}`))
