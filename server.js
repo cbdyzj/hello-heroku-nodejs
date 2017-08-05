d319494ece95b4d4ccbca2e06758e7d1f4bd483b
Cbdy <cbdyzj@gmail.com>
2017-08-05 13:52:06 +0800

    add procfile

--- server.js
+++ server.js
@@ -4,5 +4,4 @@ const http = require('http')
 
 const app = express()
 app.use(express.static(path.join(__dirname, 'public')))
-http.createServer(app).listen(8080)
-console.info('server listening 8080')
+http.createServer(app).listen(80, () => console.info('server listening'))
