7e42c4592b6c66e6115c0d02b78fde6142de3fa8
cbdyzj <cbdyzj@gmail.com>
2017-08-17 17:59:02 +0800

    add echo api

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const path = require('path')
 const http = require('http')
+const bodyParser = require('body-parser')
 
 const port = process.env.PORT || 8080
 
 const app = express()
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.all('/echo', (req, res) => {
+    const { query, body } = req
+    res.json({ query, body })
+})
 http.createServer(app).listen(port, () => console.info(`server listening ${port}`))
