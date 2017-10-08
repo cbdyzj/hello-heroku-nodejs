88dfd702f40a97ab3285a30cbd9ec39bcb8e6d01
cbdyzj <cbdyzj@gmail.com>
2017-10-08 21:56:57 +0800

    update packages

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const http = require('http')
 const bodyParser = require('body-parser')
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 3000
 
 const app = express()
 app.use(express.static(path.join(__dirname, 'public')))
