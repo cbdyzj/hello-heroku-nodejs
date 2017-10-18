abbda2bbab32f969938d5eaf7c1820f378a30184
cbdyzj <cbdyzj@gmail.com>
2017-10-18 22:08:41 +0800

    update static dir

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,12 @@ import * as Koa from 'koa'
 import * as serve from 'koa-static'
 import * as logger from 'koa-logger'
 import * as jwt from 'koa-jwt'
+import * as path from 'path'
 import route from './route'
 
 const app = new Koa
 
-app.use(serve('public'))
+app.use(serve(path.join(__dirname, '../public')))
 app.use(logger())
 // app.use(jwt({ secret: 'heroku' }))
 app.use(route.routes())
