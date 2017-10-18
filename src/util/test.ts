a9e12364f9aa603de518d1090e88e5b86f0c6e7f
cbdyzj <cbdyzj@gmail.com>
2017-10-18 22:26:10 +0800

    update test

--- src/util/test.ts
+++ src/util/test.ts
@@ -4,6 +4,5 @@ const t = {
     is: (a, b) => assert(Object.is(a, b), `current: ${a}, expected: ${b}`)
 }
 
-export async function test(cb) {
-    await cb(t)
-}
+export const test = (cb: Function) => cb(t)
+
--- test/test.ts
+++ test/test.ts
@@ -1,6 +1,6 @@
-import { test, sleep } from '../src/util'
+import { sleep, test } from '../src/util'
 
 test(async t => {
     await sleep(2000)
-    t.is(1, 2)
+    t.is(1, 1)
 })
