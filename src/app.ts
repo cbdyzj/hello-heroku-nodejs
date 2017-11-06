4b1a7e053686e337f7f0870b97ee7c613f1f6ebb
cbdyzj <cbdyzj@gmail.com>
2017-11-07 00:04:38 +0800

    rm redis

--- src/app.ts
+++ src/app.ts
@@ -4,18 +4,15 @@ import * as serve from 'koa-static'
 import * as logger from 'koa-logger'
 import * as bodyParser from 'koa-bodyparser'
 import * as session from 'koa-session'
-// import * as redisStore from 'koa-redis'
 
 import route from './route'
-
-const store = undefined // redisStore({})
 
 const app = new Koa
 app.keys = ['cbdyzj']
 app.use(logger())
 app.use(serve(path.join(__dirname, '../public')))
 app.use(bodyParser())
-app.use(session({ store }, app))
+app.use(session(app))
 app.use(route.routes())
 
 export default app
