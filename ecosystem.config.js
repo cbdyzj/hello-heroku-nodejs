ca244c45f02e120407b503bd7c92b27292814d7d
cbdyzj <cbdyzj@gmail.com>
2017-12-01 01:16:18 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,39 +1,12 @@
 module.exports = {
-  /**
-   * Application configuration section
-   * http://pm2.keymetrics.io/docs/usage/application-declaration/
-   */
-  apps : [
-    {
-      name      : 'API',
-      script    : 'app.js',
-      env: {
-        COMMON_VARIABLE: 'true'
-      },
-      env_production : {
-        NODE_ENV: 'production'
-      }
-    }
-  ],
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+  apps: [{
+    name: 'heroku.jianzhao.org',
+    script: 'server.js',
+    env: {
+      COMMON_VARIABLE: 'true'
     },
-    dev : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/development',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
-      env  : {
-        NODE_ENV: 'dev'
-      }
+    env_production: {
+      NODE_ENV: 'production'
     }
-  }
-};
+  }],
+}
