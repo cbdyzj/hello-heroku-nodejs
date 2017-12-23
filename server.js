425b9bef55181a91f1f9ba3c08e851f80397d87b
cbdyzj <cbdyzj@gmail.com>
2017-12-23 20:45:20 +0800

    amend

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
 const express = require('express')
 const path = require('path')
-
 const app = express()
-
 app.use(express.static(path.join(__dirname, 'public')))
-
 app.get('/ping', (req, res) => res.end('pong'))
-
-const port = process.env.PORT || 4200
-
-app.listen(port)
+app.listen(process.env.PORT || 4200)
